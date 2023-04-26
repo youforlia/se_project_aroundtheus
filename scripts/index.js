84227f3fb1e150602515062cb5d34ae1e93b8f00
Sarla Youforlia Castro <youforlia@Youforlia-7.local>
2023-04-26 20:21:00 +0800

    feat: add option to input name and description

--- scripts/index.js
+++ scripts/index.js
@@ -30,11 +30,76 @@ const initialCards = [
 const profileEditBtn = document.querySelector("#profile-edit-btn");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileCloseBtn = document.querySelector("#profile-close-button");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector("#profile-description-input");
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
+const cardContentEl = document.querySelector(".cards");
+
+// Functions
+
+function closePopup() {
+  profileEditModal.classList.remove("modal_opened");
+}
+
+function getCardElement(cardData) {
+// clone the template element with all its content and store it in a cardElement variable
+const cardElement = cardTemplate.cloneNode(true);
+// access the card title and image and store them in variables
+const cardImageEl = cardElement.querySelector(".cards__image");
+const cardTitleEl = cardElement.querySelector(".cards__title");
+// set the path to the image to the link field of the object
+const cardImageSrc = cardData.link;
+cardImageEl.setAttribute("src", cardImageSrc);
+// cardImageEl.setAttribute("alt", myObject.name);
+cardImageEl.setAttribute("alt", cardData.name);
+// set the card title to the name field of the object, too
+cardTitleEl.textContent = cardData.name;
+// return the ready HTML element with the filled-in data
+return cardElement;
+}
+
+
+// Event Handlers
 
 profileEditBtn.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
   profileEditModal.classList.add("modal_opened");
 });
 
+// Event Listeners
+
 profileCloseBtn.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal_opened");
+  closePopup();
 });
+
+profileEditForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
+})
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardContentEl.append(cardElement);
+});
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+

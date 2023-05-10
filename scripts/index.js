0bb8f07386ef9f75f61ecc1645305deb0441a76c
Sarla Youforlia Castro <youforlia@Youforlia-7.local>
2023-05-11 02:40:01 +0800

    feat: add add-card-modal function

--- scripts/index.js
+++ scripts/index.js
@@ -25,11 +25,15 @@ const initialCards = [
   },
 ];
 
-// Elements
-
-const profileEditBtn = document.querySelector("#profile-edit-btn");
+// Wrappers
+const cardElement = document.querySelector(".cards");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const addCardModal = document.querySelector("#add-card-modal");
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const addCardForm = addCardModal.querySelector(".modal__form");
+
+// Elements
+const profileEditBtn = document.querySelector("#profile-edit-btn");
 const profileCloseBtn = profileEditModal.querySelector(".modal__close-button");
 const addCardCloseBtn = addCardModal.querySelector(".modal__close-button");
 const profileTitle = document.querySelector(".profile__title");
@@ -38,30 +42,44 @@ const profileTitleInput = document.querySelector("#profile-title-input");
 const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
-const profileEditForm = profileEditModal.querySelector(".modal__form");
 const cardTemplate =
   document.querySelector("#card-template").content.firstElementChild;
 const cardContentEl = document.querySelector(".cards");
-const addNewCardBtn = document.querySelector(".profile__add-button");
-//const AddNewCardTitle = document.querySelector("#add-card-title-input");
-//const AddNewCardLink = document.querySelector("#add-card-link-input");
-//const AddNewCardTitleInput = document.querySelector("#add-card-title-input");
-//const AddNewCardLinkInput = document.querySelector("#add-card-link-input");
+const addCardBtn = document.querySelector(".profile__add-button");
+
+// const AddNewCardTitle = document.querySelector("#add-card-title-input");
+// const AddNewCardLink = document.querySelector("#add-card-link-input");
+const AddCardTitleInput = document.querySelector("#add-card-title-input");
+const AddCardLinkInput = document.querySelector("#add-card-link-input");
 
 // Functions
 function openPopup(popup) {
   popup.classList.add("modal_opened");
 }
 
-function handleSubmit(e) {
+function closePopup(popup) {
+  popup.classList.remove("modal_opened");
+}
+
+function renderCard(cardData) {
+  const cardElement = getCardElement(cardData);
+  cardContentEl.prepend(cardElement);
+}
+
+function handleProfileEditSubmit(e) {
   e.preventDefault();
   profileTitle.textContent = profileTitleInput.value;
   profileDescription.textContent = profileDescriptionInput.value;
-  closePopup();
+  closePopup(profileEditModal);
 }
 
-function closePopup(popup) {
-  popup.classList.remove("modal_opened");
+function handleAddCardSubmit(e) {
+  e.preventDefault();
+  const name = AddCardTitleInput.value;
+  const link = AddCardLinkInput.value;
+  renderCard({ name, link }, cardElement);
+
+  closePopup(addCardModal);
 }
 
 function getCardElement(cardData) {
@@ -81,19 +99,20 @@ function getCardElement(cardData) {
   return cardElement;
 }
 
-// Event Listeners
+// Edit Modal Listeners
 profileEditBtn.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
   openPopup(profileEditModal);
 });
-
 profileCloseBtn.addEventListener("click", () => closePopup(profileEditModal));
-profileEditForm.addEventListener("submit", handleSubmit);
-addNewCardBtn.addEventListener("click", () => openPopup(addCardModal));
+
+// Add Card Modal Listeners
+addCardBtn.addEventListener("click", () => openPopup(addCardModal));
 addCardCloseBtn.addEventListener("click", () => closePopup(addCardModal));
 
-initialCards.forEach((cardData) => {
-  const cardElement = getCardElement(cardData);
-  cardContentEl.append(cardElement);
-});
+// Form Submit Listeners
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+addCardForm.addEventListener("submit", handleAddCardSubmit);
+
+initialCards.forEach((cardData) => renderCard(cardData, cardElement));

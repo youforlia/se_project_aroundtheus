e1a51e7490b3c2db8e7e8a25a02b2f3605c20a76
youforlia <you4lia@gmail.com>
2023-11-30 01:10:59 +0800

    remove fetch

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,15 +8,6 @@ import { initialCards, config } from "../utils/constants.js";
 import "./index.css";
 
 //Request
-fetch("https://around-api.en.tripleten-services.com/v1", {
-  headers: {
-    authorization: "c56e30dc-2883-4270-a59e-b2f7bae969c6",
-  },
-})
-  .then((res) => res.json())
-  .then((result) => {
-    console.log(result);
-  });
 
 // Wrappers
 const profileEditModal = document.querySelector("#profile-edit-modal");
